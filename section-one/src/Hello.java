220055e1fd669d497d9e7297cdfccdaa7b5fd849
Frank Rauen <home@Franks-MacBook-Air.local>
2020-09-02 18:22:18 -0600

    adds to Hello

--- section-one/src/Hello.java
+++ section-one/src/Hello.java
@@ -1,6 +1,17 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, Frank");
+
+        int myFirstNumber = (5 + 5) - (6/3);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 3;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = myTotal - 1000;
+
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
     }
 }
