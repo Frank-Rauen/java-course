52bdfbd2c075e15c7f1c5baab0d3e65cacf2d7d1
Frank Rauen <home@Franks-MacBook-Air.local>
2020-09-13 12:26:29 -0600

    finishes primitive types

--- section-one/ByteShortIntLong/src/com/company/Main.java
+++ section-one/ByteShortIntLong/src/com/company/Main.java
@@ -10,8 +10,45 @@ public class Main {
         int myMaxIntValue = Integer.MAX_VALUE;
 
         System.out.println("Integer Minimum Value = " + myMinIntValue);
-        System.out.println("Integer Minimum Value = " + myMaxIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
         System.out.println("Busted Max Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Minimum Value = " + (myMinIntValue - 1));
 
+        int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+        long bigLongLiteralValue = 2_147_483_647_111L;
+
+        System.out.println(bigLongLiteralValue);
+
+        int myTotal = (myMinIntValue / 2);
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+        short myNewShortValue = (short) (myMinShortValue / 2);
+
+        byte newByte = 10;
+        short newShort = 20;
+        int newInt = 50;
+
+        long newLong = 50000L + ((newByte + newShort + newInt) * 10L );
+        System.out.println("Challenge Equals = " + newLong);
     }
 }
