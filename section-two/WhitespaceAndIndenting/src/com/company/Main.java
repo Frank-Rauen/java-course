6504990c0423ff3518602b61e8bbcef0f485c316
Frank Rauen <home@Franks-MacBook-Air.local>
2020-09-14 17:59:53 -0600

    adds codeblock

--- section-two/WhitespaceAndIndenting/src/com/company/Main.java
+++ section-two/WhitespaceAndIndenting/src/com/company/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	int myVariable = 50;
+
+	if(myVariable == 50) {
+        System.out.println("myVariable is 50");
+    }
+
 	myVariable++;
 	myVariable--;
 	System.out.println("This is a test");
