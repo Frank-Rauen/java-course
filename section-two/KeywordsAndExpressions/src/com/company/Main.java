4b504c134259a7090763a3997fb5eef9454112ac
Frank Rauen <home@Franks-MacBook-Air.local>
2020-09-14 17:29:55 -0600

    adds section two

---
+++ section-two/KeywordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// mile -> kilometer = 1.609344
+    double kilometers = (100 * 1.609344);
+
+    System.out.println("100 Miles equals " + kilometers);
+
+    int highScore = 50;
+
+    if(highScore == 50) {
+        System.out.println("This is an expression");
+        }
+     
+
+    }
+}
