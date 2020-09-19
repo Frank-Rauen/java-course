5b7f668c3a5660bda0d52ffee0c4f18856261750
Frank Rauen <home@Franks-MacBook-Air.local>
2020-09-18 20:50:29 -0600

    solves user input challenge

---
+++ section-three/ReadingUserInput/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Year of Birth: ");
+
+        boolean hasNextInt = scanner.hasNextInt();
+
+        if(hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2020 - yearOfBirth;
+
+                if(age >= 0 && age <= 150) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old");
+                } else {
+                System.out.println("Invalid Entry");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth");
+        }
+
+        scanner.close();
+    }
+}
