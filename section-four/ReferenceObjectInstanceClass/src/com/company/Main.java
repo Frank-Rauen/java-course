a145762d224bdd472e08b360904fefa593dde17c
Frank Rauen <home@Franks-MacBook-Air.local>
2020-09-22 17:55:09 -0600

    adds cheat sheet

---
+++ section-four/ReferenceObjectInstanceClass/src/com/company/Main.java
@@ -0,0 +1,11 @@
+//Using a house as an example
+
+//Class
+//A class is basically the blueprint for a house
+//        (fields, and methods)
+
+//Object
+//An object is each INSTANCE/Instantiation of the class
+
+//Reference
+//References the location but doesn't create a new instance
