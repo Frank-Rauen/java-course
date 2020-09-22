1d38268f4f2693f59da8a8b85d8de972934e7f63
Frank Rauen <home@Franks-MacBook-Air.local>
2020-09-21 19:36:23 -0600

    adds constructor challenges

---
+++ section-four/ConstructorsChallenges/src/com/company/Floor.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Floor {
+    private double width;
+    private double length;
+
+    public Floor(double width, double length) {
+        this.width = width < 0 ? 0 : width;
+        this.length = length < 0 ? 0 : length;
+    }
+    public double getArea() {
+        return this.width * this.length;
+    }
+}
+
+public class Carpet {
+    private double cost;
+
+    public Carpet(double cost) {
+        this.cost = cost < 0 ? 0 : cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
+
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+    public double getTotalCost() {
+        return floor.getArea() * carpet.getCost();
+    }
+}
