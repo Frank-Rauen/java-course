b96e87d2119a96c465efc70f6cfa15f2d93f4a6e
Frank Rauen <home@Franks-MacBook-Air.local>
2020-09-22 19:34:43 -0600

    adds Inheritance challenge

---
+++ section-four/InheritanceChallenges/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SportsCar carrera = new SportsCar(48);
+        carrera.accelerate(40);
+    }
+}
