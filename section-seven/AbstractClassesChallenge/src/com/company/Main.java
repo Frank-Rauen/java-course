3b0b28a5fa02b6e4ae6d904044a500cb3830e75e
Frank Rauen <home@Franks-MacBook-Air.local>
2020-09-29 12:21:55 -0600

    Adds abstract challenges

--- section-seven/AbstractClassesChallenge/src/com/company/Main.java
+++ section-seven/AbstractClassesChallenge/src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        SearchTree tree = new SearchTree(null);
+        tree.traverse(tree.getRoot());
+
+	    String stringData = "1 2 3 4 5 6 7 8 9 10 ";
+
+	    String[] data = stringData.split(" ");
+	    for (String s : data) {
+            tree.addItem(new Node(s));
+        }
+
+//	    tree.traverse(tree.getRoot());
     }
 }
