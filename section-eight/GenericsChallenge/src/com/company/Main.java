c198b41d604ab11c2d3e3d030a6f9b8231f25716
Frank Rauen <home@Franks-MacBook-Air.local>
2020-10-06 16:55:41 -0600

    adds naming conventions

--- section-eight/GenericsChallenge/src/com/company/Main.java
+++ section-eight/GenericsChallenge/src/com/company/Main.java
@@ -16,7 +16,7 @@ public class Main {
         footballLeague.add(jets);
         footballLeague.add(giants);
 
-        
+
 
 
 
---
+++ section-nine/NamingConventions/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+Packages:
+        always lower case; unique; reversed domain name as prefix
+Class Names:
+        CamelCase; nouns; capitalized
+Interface:
+        CamelCase; descriptive name;
+ Methods:
+        mixedCase; verbs; reflect function
+Constants:
+        all Upper Case; underscore separator; final
+Variables:
+        mixedCase; descriptive; no underscores;
+*/
+    }
+}
