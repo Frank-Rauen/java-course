a4e93cae38072835d45278fe706e48763e36abe8
Frank Rauen <home@Franks-MacBook-Air.local>
2020-10-08 21:06:28 -0600

    adds map work

--- section-ten/Adventure/src/com/company/Main.java
+++ section-ten/Adventure/src/com/company/Main.java
@@ -10,28 +10,45 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        locations.put(0, new Location(0, "At home in the Shire"));
-        locations.put(1, new Location(1, "Mines of Moria"));
-        locations.put(2, new Location(2, "Rivendell"));
-        locations.put(3, new Location(3, "Rohan"));
-        locations.put(4, new Location(4, "Gondor"));
-        locations.put(5, new Location(5, "Mordor"));
+        Map<String, Integer> tempExit = new HashMap<String, Integer>();
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
+        locations.put(0, new Location(0, "At home in the Shire", tempExit));
 
-        locations.get(2).addExit("N", 5);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W", 2);
+        tempExit.put("E", 3);
+        tempExit.put("S", 4);
+        tempExit.put("N", 5);
+        locations.put(1, new Location(1, "Mines of Moria", tempExit));
 
-        locations.get(3).addExit("W", 1);
 
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N", 5);
+        locations.put(2, new Location(2, "Rivendell", tempExit));
 
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
 
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W", 1);
+        locations.put(3, new Location(3, "Rohan", tempExit));
+
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N", 1);
+        tempExit.put("W", 2);
+        locations.put(4, new Location(4, "Gondor", tempExit));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("S", 1);
+        tempExit.put("W", 2);
+        locations.put(5, new Location(5, "Mordor", tempExit));
+
+
+        Map<String, String> vocabulary = new HashMap<String, String>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("EAST", "E");
+        vocabulary.put("WEST", "W");
 
         int loc = 1;
         while(true) {
@@ -47,6 +64,15 @@ public class Main {
             System.out.println();
 
             String direction = s.nextLine().toUpperCase();
+            if(direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for(String word : words) {
+                    if(vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
 
             if(exits.containsKey(direction)) {
                 loc = exits.get(direction);
