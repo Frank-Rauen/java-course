6cd50a472cd332f359d8e17fd252aa6056f3e096
Frank Rauen <home@Franks-MacBook-Air.local>
2020-10-20 20:27:18 -0600

    adds events and event handlers

--- section-eleven/Events/src/sample/Controller.java
+++ section-eleven/Events/src/sample/Controller.java
@@ -1,7 +1,59 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
 public class Controller {
-    public void onButtonClicked() {
-        System.out.println("Hello");
+    @FXML
+    private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
+    @FXML
+    private CheckBox ourCheckBox;
+
+    public void initialize() {
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
+    public void onButtonClicked(ActionEvent e) {
+        if(e.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        } else if(e.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException event) {
+            
+        }
+
+        if(ourCheckBox.isSelected()) {
+            nameField.clear();
+//            handleKeyReleased();
+//            initialize();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased() {
+        String text = nameField.getText();
+        boolean disabledButtons = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disabledButtons);
+        byeButton.setDisable(disabledButtons);
+    }
+
+    @FXML
+    public void handleChange() {
+        System.out.println("Checkbox is " + (ourCheckBox.isSelected() ? "checked" : "unchecked"));
     }
 }
