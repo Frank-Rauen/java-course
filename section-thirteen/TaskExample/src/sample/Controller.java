0aa9bf9b4b2f013a910e7d7fe1714497e343f27f
Frank Rauen <home@Franks-MacBook-Air.local>
2020-11-02 17:09:44 -0700

    adds UI threads and concurrency example

---
+++ section-thirteen/TaskExample/src/sample/Controller.java
@@ -0,0 +1,65 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+
+public class Controller {
+
+    @FXML
+    private ListView listView;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private Label progressLabel;
+
+    private Service<ObservableList<String>> service;
+
+    public void initialize() {
+//        task = new Task<ObservableList<String>>() {
+//            @Override
+//            protected ObservableList<String> call() throws Exception {
+//                String[] names = {"Frank Rauen",
+//                        "Slade Webb",
+//                        "Jack Thomson",
+//                        "Nate Diaz",
+//                        "Bob DaBuilder"};
+//
+//                final ObservableList<String> employees = FXCollections.observableArrayList();
+//
+//                for(int i = 0; i < 5; i++) {
+//                    employees.add(names[i]);
+//                    updateMessage("Added " + names[i] + " to the list");
+//                    updateProgress(i + i, 5);
+//                    Thread.sleep(200);
+//                }
+//
+//                return employees;
+//
+//            }
+//        };
+
+        service = new EmployeeService();
+
+        progressBar.progressProperty().bind(service.progressProperty());
+        progressLabel.textProperty().bind(service.messageProperty());
+        listView.itemsProperty().bind(service.valueProperty());
+
+    }
+
+    @FXML
+    public void buttonPressed() {
+        if(service.getState() == Service.State.SUCCEEDED) {
+            service.reset();
+            service.start();
+        } else if(service.getState() == Service.State.READY) {
+            service.start();
+        }
+    }
+}
